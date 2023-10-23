c1f0d973174399e8d6efb91e7edd214251fcf83d
Andrey Ivanov <this.andrey@gmail.com>
2023-10-23 18:27:42 +0300

    init

---
+++ lib/ar_cli.dart
@@ -0,0 +1,10 @@
+/// ar_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate ar_cli
+/// dart pub global activate ar_cli
+///
+/// # see usage
+/// ar_cli --help
+/// ```
+library ar_cli;
